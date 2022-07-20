85bb5fb82487137c60283626aab57e8c538e65ce
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2022-07-20 09:40:29 +0300

    adds changes to all files

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-//==========================Total Production(litres)===========================>
-let total = 0 // shed1, shed2, shed3, shed4 //
+
+let week = 0;
+let month = 0;
 const data = {
-    'shed1': [], // shed one
-    'shed2': [],  // shed two
-    'shed3': [],
-    'shed4': [],
+    'shed1': [], // shed one//
+    'shed2': [],  // shed two//
+    'earnings': [], // total shed production//
+    'monthEarnings': [],
+    'months': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
 }
 
-//=================================table========================>
-function getTable() {
-    let tr1 = document.getElementsByClassName('sheds')
-    let tr2 = document.getElementsByClassName('Litres')
-    let tr3 = document.getElementsByClassName('Amount')
-    let tr4 = document.getElementsByClassName('No.of Cows')
+// create table
+function createTable() {
+    let tr1 = document.getElementById('shed1')
+    let tr2 = document.getElementById('shed2')
+    let tr3 = document.getElementById('total')
+    let tr4 = document.getElementById('earnings')
     for (let x = 0; x < 7; x++) {
-        let td1 = document.getElementsByClassName('td')
-        let td2 = document.getElementsByClassName('td')
-        let td3 = document.getElementsByClassName('td')
-        let td4 = document.getElementsByClassName('td')
-        td1.setAttribute('id', `shedOne${x}`)
-        td2.setAttribute('id', `shedTwo${x}`)
+        let td1 = document.createElement('td')
+        let td2 = document.createElement('td')
+        let td3 = document.createElement('td')
+        let td4 = document.createElement('td')
+        td1.setAttribute('id', `shed1${x}`)
+        td2.setAttribute('id', `shed2${x}`)
         td3.setAttribute('id', `total${x}`)
         td4.setAttribute('id', `earnings${x}`)
         tr1.appendChild(td1)
@@ -28,36 +31,35 @@ function getTable() {
         tr4.appendChild(td4)
     }
 }
-
 // function to generate random values between 10 & 15 and fill shedOne & shedTwo arrays
 const randGen = () => {
     for (let i = 0; i < 366; i++) {
         let a = Math.floor(Math.random() * (15 - 10) + 10);
         let b = Math.floor(Math.random() * (15 - 10) + 10);
-        data.shedOne.push(a)
-        data.shedTwo.push(b)
+        data.shed1.push(a)
+        data.shed2.push(b)
         data.earnings.push((a + b) * 45)
     }
     console.log(data)
 }
 
-const fillShedOne = () => {
+const fillShed1 = () => {
     for (let x = week; x < 7 + week; x++) {
-        let td = document.getElementById(`shedOne${x - week}`)
-        td.innerHTML = data.shedOne[x]
+        let td = document.getElementById(`shed1${x - week}`)
+        td.innerHTML = data.shed1[x]
     }
 }
 
-const fillShedTwo = () => {
+const fillShed2 = () => {
     for (let x = week; x < 7 + week; x++) {
-        let td = document.getElementById(`shedTwo${x - week}`)
-        td.innerHTML = data.shedTwo[x]
+        let td = document.getElementById(`shed2${x - week}`)
+        td.innerHTML = data.shed2[x]
     }
 }
 
 const fillTotal = () => {
     for (let x = week; x < 7 + week; x++) {
-        let total = data.shedOne[x] + data.shedTwo[x]
+        let total = data.shed1[x] + data.shed2[x]
         let td = document.getElementById(`total${x - week}`)
         td.innerHTML = total
     }
@@ -70,39 +72,63 @@ const fillEarnings = () => {
     }
 }
 
+const monthReport = () => {
+    let monthTotal = 0;
+    for (let x = 0; x < 366; x++) {
+        if (x % 30 === 0 && x > 0) {
+            data.monthEarnings.push(monthTotal)
+            monthTotal = 0;
+        } else {
+            monthTotal += data.earnings[x]
+        }
+    }
+}
+
 const totalProduction = () => {
     randGen()
     createTable()
-    fillShedOne()
-    fillShedTwo()
+    fillShed1()
+    fillShed2()
     fillTotal()
     fillEarnings()
+    monthReport()
     let weekIndicator = document.getElementById('week')
     let next = document.getElementById('next')
     let previous = document.getElementById('previous')
     weekIndicator.innerHTML = `Week ${(week / 7) + 1}`
+    let mReport = document.getElementById('month-report')
+    mReport.innerHTML = `Your income for ${data.months[month]} is ${data.monthEarnings[month]}`
     next.onclick = () => {
         if (week < (51 * 7)) {
             week += 7
-            fillShedOne()
-            fillShedTwo()
+            month += 0.25
+            fillShed1()
+            fillShed2()
             fillTotal()
             fillEarnings()
             let weekIndicator = document.getElementById('week')
             weekIndicator.innerHTML = `Week ${(week / 7) + 1}`
+            let mReport = document.getElementById('month-report')
+            mReport.innerHTML = `Your income for ${data.months[Math.floor(month)]} is
+             ${data.monthEarnings[Math.floor(month)]}`
         }
     }
     previous.onclick = () => {
         if (week > 0) {
             week -= 7
-            fillShedOne()
-            fillShedTwo()
+            month -= 0.25
+            fillShed1()
+            fillShed2()
             fillTotal()
             fillEarnings()
             let weekIndicator = document.getElementById('week')
             weekIndicator.innerHTML = `Week ${(week / 7) + 1}`
+            let mReport = document.getElementById('month-report')
+            mReport.innerHTML = `Your income for ${data.months[Math.floor(month)]} 
+            is ${data.monthEarnings[Math.floor(month)]}`
         }
     }
 }
 
 totalProduction()
+
